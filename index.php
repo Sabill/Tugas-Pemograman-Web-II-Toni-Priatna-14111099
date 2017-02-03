cdd04559b93b41d644291be92d2c9981cf7f83f4
Sabill <priatnacorp@gmail.com>
2017-02-03 18:16:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pemograman Web II</title>
+	<link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css">
+	<link href="favicon.png" rel="icon" type="image/png" />
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-static-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        </button>
+		        <a href="milto:priatnacorp@gmail.com/" class="navbar-brand">Tugas <b>1</b></a>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<table class="table table-nobordered">
+			<thead>
+				<tr align="center">
+					<td>Nama</td>
+					<td>Toni Priatna</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr align="center">
+					<td>NIM</td>
+					<td>14 111 099</td>
+				</tr>
+				<tr align="center">
+					<td>Kelas</td>
+					<td>TIF RM 14</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+<script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
